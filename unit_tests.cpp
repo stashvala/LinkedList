4e8086df58d3e8c41715a4350735301524f3abaa
stashvala <stas.hvala@gmail.com>
2016-08-12 14:59:00 +0200

    added .gitignore, updated unit_tests.cpp

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -2,31 +2,36 @@
 
 TEST(testAppend, appendIntToEmptyList) {
     LinkedList<int> list;
+    
     list.append(1);
-
+    
     ASSERT_EQ(list[0], 1);
 }
 
 TEST(testAppend, appendDoubleToEmptyList) {
     LinkedList<double> doubleList;
+    
     doubleList.append(1.618);
     ASSERT_EQ(doubleList[0], 1.618);
 }
 
 TEST(testAppend, appendCharToEmptyList) {
     LinkedList<char> charList;
+    
     charList.append('a');
     ASSERT_EQ(charList[0], 'a');
 }
 
 TEST(testAppend, appendBoolToEmptyList) {
     LinkedList<bool> boolList;
+    
     boolList.append(true);
     ASSERT_EQ(boolList[0], true);
 }
 
 TEST(testAppend, appendSecondInt) {
     LinkedList<int> list;
+    
     list.append(1);
     list.append(2);
 
@@ -54,6 +59,7 @@ TEST(testAppend, appendMultipleInts) {
 
 TEST(testAppend, appendDuplicate) {
     LinkedList<int> list;
+    
     list.append(1);
     list.append(1);
 
@@ -62,6 +68,7 @@ TEST(testAppend, appendDuplicate) {
 
 TEST(testRemove, removeFromOneIntList) {
     LinkedList<int> list;
+    
     list.append(1);
     list.remove(1);
 
@@ -70,6 +77,7 @@ TEST(testRemove, removeFromOneIntList) {
 
 TEST(testRemove, removeFromOneDoubleList) {
     LinkedList<double> list;
+    
     list.append(1.618);
     list.remove(1.618);
 
@@ -78,6 +86,7 @@ TEST(testRemove, removeFromOneDoubleList) {
 
 TEST(testRemove, removeFromOneCharList) {
     LinkedList<char> list;
+    
     list.append('a');
     list.remove('a');
 
@@ -86,42 +95,82 @@ TEST(testRemove, removeFromOneCharList) {
 
 TEST(testRemove, removeFromOneBoolList) {
     LinkedList<bool> list;
+    
     list.append(true);
     list.remove(true);
 
     ASSERT_EQ(list.size(), 0);
 }
 
-TEST(testRemove, removeSecondFromTwoElementList) {
+TEST(testRemove, removeSecondFromTwoIntList) {
     LinkedList<int> list;
+    
     list.append(1);
     list.append(2);
     list.remove(2);
 
     ASSERT_EQ(list.size(), 1);
+    ASSERT_TRUE(list[0] == 1);
 }
 
-TEST(testRemove, removeFirstFromTwoElementList) {
-
+TEST(testRemove, removeFirstFromTwoIntList) {
+    LinkedList<int> list;
+    
+    list.append(1);
+    list.append(2);
+    list.remove(1);
+    
+    ASSERT_EQ(list.size(), 1);
+    ASSERT_EQ(list[0], 2);
 }
 
 TEST(testRemove, removeMiddleFromThreeElementList) {
-
+    LinkedList<int> list;
+    
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.remove(2);
+    
+    ASSERT_EQ(list.size(), 2);
+    ASSERT_EQ(list[0], 1);
+    ASSERT_EQ(list[1], 3);
 }
 
 TEST(testRemove, removeMultipleElements) {
+    LinkedList<int> list;
 
+    for (int i = 0; i < 100; i++) {
+        list.append(i);
+    }
+    
+    for(int i = 0; i < 100; i++) {
+	list.remove(0);
+    }
+    
+    ASSERT_EQ(list.size(), 0);
 }
 
 TEST(testRemove, removeFromEmptyList) {
-
+    LinkedList<int> list;
+    
+    ASSERT_DEATH(list.remove(1), "*");
 }
 
 TEST(testRemove, removeNonexistentElement) {
-
+    LinkedList<int> list;
+    list.append(1);
+    list.append(2);
+  
+    ASSERT_DEATH(list.remove(3), "*");
 }
 
 TEST(testRemove, removeDuplicates) {
-
+    LinkedList<int> list;
+    list.append(1);
+    list.append(1);
+    list.remove(1);
+    
+    ASSERT_EQ(list.size(), 0);
 }
 
